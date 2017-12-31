1d0190e4f5feb940299325d0e0c2c378aa385e46
Christoffer Assgård <a.christoffer@outlook.com>
2017-12-31 16:54:03 +0100

    fix trizen path

--- tools/aur.js
+++ tools/aur.js
@@ -6,7 +6,7 @@ const constants = require('../constants/tools');
 exports.install = (pkg, callback) => {
     if (!pkg) { callback({'msg': 'Pkg cannot be null'}); }
     // Call cli to install with pacman
-    cmd.start("./tools/trizen", ['-S', pkg, '--nocolors'], function(err, data) {
+    cmd.start("perl", [__dirname+'/trizen', '-Sy', pkg, '--nocolors'], function(err, data) {
         if (err) { 
             callback({'msg': 'Error when trying to install: ' + pkg + " -> " + err }); // TODO: Not needed?
         } else {
@@ -26,7 +26,7 @@ exports.restore = (pkgs, callback) => {
     //        callback(); // TODO: Send the data back?
     //    }
     //});
-    cmd.start("./tools/trizen", ['-Sy', '--noconfirm', '--nocolors'].concat(pkgs.map(x => x.name)), function(err, data) {
+    cmd.start("perl", [__dirname+"/trizen", '-Sy', '--noconfirm', '--nocolors'].concat(pkgs.map(x => x.name)), function(err, data) {
         if (err) { 
             callback({'msg': 'Error when trying to install: ' + pkg + " -> " + err });
         } else {
@@ -45,7 +45,7 @@ exports.update = (callback) => {
     //        callback();
     //    }
     //});
-    cmd.start("./tools/trizen", ['-Syu', '--noconfirm', '--nocolors'], function(err, data) {
+    cmd.start("perl", [__dirname+"/trizen", '-Syu', '--noconfirm', '--nocolors'], function(err, data) {
         console.log("Back from trizen!")
         if (err) { 
             callback({'msg': 'Error when trying to update with tryzen: ' + " -> " + err });
