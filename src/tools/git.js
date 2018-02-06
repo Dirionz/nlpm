e53038e0b3e0b99ab4b850b51c3b70552ae6ac05
Christoffer Assgård <a.christoffer@outlook.com>
2018-02-06 17:06:09 +0100

    Cleanups

--- src/tools/git.js
+++ src/tools/git.js
@@ -1,53 +1,53 @@
-'use strict'
-var cmd=require('./cmd');
-const mkdirp = require('mkdirp');
-const getDirName = require('path').dirname;
-const config = require("../config/config")
+// 'use strict'
+// var cmd=require('./cmd');
+// const mkdirp = require('mkdirp');
+// const getDirName = require('path').dirname;
+// const config = require("../config/config")
 
-exports.install = (pkg, callback) => {
-    if (!pkg) { callback({'msg': 'Pkg cannot be null'}); }
+// exports.install = (pkg, callback) => {
+//     if (!pkg) { callback({'msg': 'Pkg cannot be null'}); }
 
-    mkdirp(getDirName(config.get().gitAppDir+pkg), function (err) {
-        if (err) return callback(err);
+//     mkdirp(getDirName(config.get().gitAppDir+pkg), function (err) {
+//         if (err) return callback(err);
 
-        cmd.start('git', ['clone', 'https://github.com/'+pkg, config.get().gitAppDir+pkg], function(err, data) {
-            if (err) { 
-                callback({'msg': 'Error when trying to install: ' + pkg + " -> " + err });
-            } else {
-                callback();
-            }
-        });
+//         cmd.start('git', ['clone', 'https://github.com/'+pkg, config.get().gitAppDir+pkg], function(err, data) {
+//             if (err) { 
+//                 callback({'msg': 'Error when trying to install: ' + pkg + " -> " + err });
+//             } else {
+//                 callback();
+//             }
+//         });
         
-    });
+//     });
 
-}
+// }
 
-exports.restore = (pkg, callback) => {
-    mkdirp(getDirName(config.get().gitAppDir+pkg), function (err) {
-        if (err) return callback(err);
+// exports.restore = (pkg, callback) => {
+//     mkdirp(getDirName(config.get().gitAppDir+pkg), function (err) {
+//         if (err) return callback(err);
 
-        cmd.run('git clone https://github.com/' + pkg + " " + config.get().gitAppDir+pkg, function(err, data) {
-            if (err) { 
-                callback({'msg': pkg + " -> " + err });
-            } else {
-                callback();
-            }
-        });
+//         cmd.run('git clone https://github.com/' + pkg + " " + config.get().gitAppDir+pkg, function(err, data) {
+//             if (err) { 
+//                 callback({'msg': pkg + " -> " + err });
+//             } else {
+//                 callback();
+//             }
+//         });
         
-    });
-}
+//     });
+// }
 
-exports.update = (pkg, callback) => {
-    mkdirp(getDirName(config.get().gitAppDir+pkg), function (err) {
-        if (err) return callback(err);
+// exports.update = (pkg, callback) => {
+//     mkdirp(getDirName(config.get().gitAppDir+pkg), function (err) {
+//         if (err) return callback(err);
 
-        cmd.run('git -C ' + config.get().gitAppDir+pkg + ' ' + 'pull', function(err, data) {
-            if (err) { 
-                callback({'msg': pkg + " -> " + err });
-            } else {
-                callback();
-            }
-        });
+//         cmd.run('git -C ' + config.get().gitAppDir+pkg + ' ' + 'pull', function(err, data) {
+//             if (err) { 
+//                 callback({'msg': pkg + " -> " + err });
+//             } else {
+//                 callback();
+//             }
+//         });
         
-    });
-}
+//     });
+// }
