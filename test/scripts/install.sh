10cc1c05da94cbd62546742a7abd3652d89fc587
Dirionz <a.christoffer@outlook.com>
2019-04-14 19:31:50 +0200

    Skip arch install.sh

--- test/scripts/install.sh
+++ test/scripts/install.sh
@@ -4,6 +4,6 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker build . -t $DOCKER_USERNAME/nlpm:ubuntu -f test/docker/ubuntu/Dockerfile
   docker push $DOCKER_USERNAME/nlpm:ubuntu;
-  docker build . -t $DOCKER_USERNAME/nlpm:arch -f test/docker/arch/Dockerfile
-  docker push $DOCKER_USERNAME/nlpm:arch;
+  #docker build . -t $DOCKER_USERNAME/nlpm:arch -f test/docker/arch/Dockerfile
+  #docker push $DOCKER_USERNAME/nlpm:arch;
 fi
