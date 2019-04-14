ae72a35cfdd89d0870e37000ca376fd9959323af
Dirionz <a.christoffer@outlook.com>
2019-04-14 19:32:20 +0200

    Skip arch run.sh

--- test/scripts/run.sh
+++ test/scripts/run.sh
@@ -3,5 +3,5 @@ set -ev
 npm test
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     docker run --user user $DOCKER_USERNAME/nlpm:ubuntu /bin/bash -c "source ~/.nvm/nvm.sh; cd /home/user/nlpm; ./src/nlpm.js restore;"
-    docker run --user user $DOCKER_USERNAME/nlpm:arch /bin/bash -c "source ~/.nvm/nvm.sh; cd /home/user/nlpm; ./src/nlpm.js restore;"
+    #docker run --user user $DOCKER_USERNAME/nlpm:arch /bin/bash -c "source ~/.nvm/nvm.sh; cd /home/user/nlpm; ./src/nlpm.js restore;"
 fi
